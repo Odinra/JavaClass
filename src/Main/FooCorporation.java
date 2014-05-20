8787592b1a4b995dc7924b32d11ab851544e44a6
Odinra <ckking70@gmail.com>
2014-05-20 15:24:28 -0500

    Auto Formatted the file usinf ctrl-shift-f

--- src/Main/FooCorporation.java
+++ src/Main/FooCorporation.java
@@ -5,32 +5,30 @@ package Main;
 
 /**
  * @author CK
- *
+ * 
  */
 public class FooCorporation {
 
 	/**
 	 * @param args
 	 */
-	public static void Pay(double hours, double pay){
+	public static void Pay(double hours, double pay) {
 		double totalPay = 0;
-		if(pay < 8.00){
-			System.out.println("Error: You must be payed at least $8.00 per hour");
-		}
-		else if (hours <= 60){
+		if (pay < 8.00) {
+			System.out
+					.println("Error: You must be payed at least $8.00 per hour");
+		} else if (hours <= 60) {
 			totalPay = hours * pay;
-			if(hours > 40){
+			if (hours > 40) {
 				totalPay += (hours - 40) * pay * .5;
 			}
 			System.out.println("Amount to be paid: $" + totalPay);
-		}
-		else{
+		} else {
 			System.out.println("Error, Worked over 60 hours this week");
 		}
-		
 
-		
 	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Pay(35, 7.50);
