c4249b5e1e88788f3a8b9a014c2e51aa513359c7
Odinra <ckking70@gmail.com>
2014-05-20 20:19:34 -0500

    Finished Project 5

--- src/Main/DrawGraphics.java
+++ src/Main/DrawGraphics.java
@@ -4,11 +4,16 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 public class DrawGraphics {
-    BouncingBox box;
+    BouncingBox box, box2, box3;
     
     /** Initializes this class for drawing. */
     public DrawGraphics() {
         box = new BouncingBox(200, 50, Color.RED);
+        box.setMovementVector(2, 1);
+        box2 = new BouncingBox(50, 200, Color.BLUE);
+        box2.setMovementVector(1, 2);
+        box3 = new BouncingBox(100, 100, Color.GREEN);
+        box3.setMovementVector(2, 5);
     }
 
     /** Draw the contents of the window on surface. Called 20 times per second. */
@@ -21,5 +26,7 @@ public class DrawGraphics {
         surface.drawRoundRect(100, 200, 50, 100, 20, 20);
         
         box.draw(surface);
+        box2.draw(surface);
+        box3.draw(surface);
     }
 } 
