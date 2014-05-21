84271f0f9a84ab275900a2155f44c5cd0be15d34
Odinra <ckking70@gmail.com>
2014-05-20 21:27:00 -0500

    Finished Project 6: Graphics strike back!

--- src/Main/Circle.java
+++ src/Main/Circle.java
@@ -5,24 +5,24 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 
-public class Circle implements Sprite{
+public class Circle implements Sprite {
 	private int radius;
 	private Color color;
-	
-	public Circle(int radius, Color color){
+
+	public Circle(int radius, Color color) {
 		this.radius = radius;
 		this.color = color;
 	}
-	
-	public void draw(Graphics surface, int x, int y){
+
+	public void draw(Graphics surface, int x, int y) {
 		surface.setColor(color);
 		surface.fillOval(x, y, radius, radius);
 		surface.setColor(Color.BLACK);
 		((Graphics2D) surface).setStroke(new BasicStroke(5.0f));
 		surface.drawOval(x, y, radius, radius);
 	}
-	
-	public int getRadius(){
+
+	public int getRadius() {
 		return radius;
 	}
 
--- src/Main/Mover.java
+++ src/Main/Mover.java
@@ -1,5 +1,11 @@
 package Main;
 
+import java.awt.Graphics;
+
 public interface Mover {
+
+	void draw(Graphics surface);
+
+	void setMovementVector(int i, int j);
 
 }
